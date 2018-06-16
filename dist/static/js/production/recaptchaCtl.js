914408eaa52b0a95f3404ceb8f119c3d5b7237c3
halversonmd <halversonmd@gmail.com>
2018-06-16 13:19:56 -0400

    ga tags

---
+++ dist/static/js/production/recaptchaCtl.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var onSuccess = async function (token) {
+  var subButton = document.getElementById('contactSubmit')
+  var xhr = new XMLHttpRequest();
+  var url = "https://openlocation.codes/api/verify";
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  var data = JSON.stringify({"token": token});
+  var success;
+  var promise = await new Promise((resolve, reject) => { 
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          var json = JSON.parse(xhr.responseText);
+          resolve( success = json.success )
+      }
+    };
+    xhr.send(data);
+  });
+  if ( success ) {
+    subButton.disabled = false
+  }
+  return success
+}
